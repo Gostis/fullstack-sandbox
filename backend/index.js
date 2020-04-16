a1831cdd64f3b716806032813dc69d675f9f9dff
Jacob Nyman <jacob.nyman@angrycreative.com>
2020-04-16 21:09:37 +0200

    dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-const PORT = 3001
+const PORT = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello World1!"));
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
